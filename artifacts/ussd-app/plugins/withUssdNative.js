8a712c9f59e95f72b318ca2f5544c0d8708db584
monestw-cell <monestw@gmail.com>
2026-04-29 01:20:46 +0300

    fix: import config-plugins via 'expo/config-plugins' re-export (SDK 54 compat)

--- artifacts/ussd-app/plugins/withUssdNative.js
+++ artifacts/ussd-app/plugins/withUssdNative.js
@@ -2,7 +2,7 @@ const {
   withAndroidManifest,
   withMainApplication,
   withDangerousMod,
-} = require('@expo/config-plugins');
+} = require('expo/config-plugins');
 const path = require('path');
 const fs = require('fs');
 
