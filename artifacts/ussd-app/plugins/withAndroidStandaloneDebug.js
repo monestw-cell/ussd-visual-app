d5327f52b58ca1b99b5763d1d7a2bd6dcdb92186
monestw-cell <monestw@gmail.com>
2026-04-29 10:13:05 +0300

    fix(android): simplify plugin - only do ABI splits, rely on Expo default debug keystore signing for release

--- artifacts/ussd-app/plugins/withAndroidStandaloneDebug.js
+++ artifacts/ussd-app/plugins/withAndroidStandaloneDebug.js
@@ -11,15 +11,6 @@ const ABI_SPLITS_BLOCK = `
     }
 `;
 
-const RELEASE_SIGNING_BLOCK = `
-        release {
-            storeFile file('debug.keystore')
-            storePassword 'android'
-            keyAlias 'androiddebugkey'
-            keyPassword 'android'
-        }
-`;
-
 module.exports = function withAndroidStandaloneDebug(config) {
   return withAppBuildGradle(config, (cfg) => {
     let g = cfg.modResults.contents;
@@ -27,18 +18,6 @@ module.exports = function withAndroidStandaloneDebug(config) {
     if (g.includes('android {') && !g.includes('splits {')) {
       g = g.replace(/android\s*\{/, 'android {' + ABI_SPLITS_BLOCK);
     }
-
-    if (g.includes('signingConfigs {') && !g.match(/signingConfigs\s*\{[\s\S]*?release\s*\{/)) {
-      g = g.replace(
-        /signingConfigs\s*\{/,
-        'signingConfigs {' + RELEASE_SIGNING_BLOCK
-      );
-    }
-
-    g = g.replace(
-      /(buildTypes\s*\{[\s\S]*?release\s*\{[\s\S]*?signingConfig\s+signingConfigs\.)debug/,
-      '$1release'
-    );
 
     cfg.modResults.contents = g;
     return cfg;
